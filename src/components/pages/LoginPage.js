8d56d450644745b427e8407937013ec7ec1383f2
NiceAndFlowy <NiceAndFlowy@user.noreply.github.com>
2017-10-10 17:21:42 -0400

    Added login with error checking

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import Login from '../Login';
 
-const LoginPage = () => {
-  return (
-    <div>
-      <h1>LoginPage!</h1>  
-      <Login />
-    </div>
-  );
-};
+
+class LoginPage extends Component {
+  onSubmit = () => {
+    console.log('submitting data');
+  }
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <Login submit={this.onSubmit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
